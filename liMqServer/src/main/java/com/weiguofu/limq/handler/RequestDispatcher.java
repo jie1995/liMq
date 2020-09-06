9422935a33f68bc4cad91e1ad7d36673309ff047
15173 <15173126889@163.com>
2020-09-06 19:48:23 +0800

    优化反射调用

--- liMqServer/src/main/java/com/weiguofu/limq/handler/RequestDispatcher.java
+++ liMqServer/src/main/java/com/weiguofu/limq/handler/RequestDispatcher.java
@@ -1,7 +1,6 @@
 package com.weiguofu.limq.handler;
 
 import com.google.gson.Gson;
-import com.weiguofu.limq.ResponseUtil;
 import com.weiguofu.limq.messageDto.MessageWrapper;
 import com.weiguofu.limq.messageDto.RequestMessage;
 import com.weiguofu.limq.service.LimqRequestReceive;
@@ -23,20 +22,13 @@ public class RequestDispatcher {
 
     public Object requestHandle(MessageWrapper mw) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         String message = mw.getMessage();
-        log.info("message:{}", message);
         Gson gson = new Gson();
         RequestMessage rm = gson.fromJson(message, RequestMessage.class);
         Class<LimqRequestReceive> clazz = (Class<LimqRequestReceive>) Class.forName(rm.getClassName());
-        Method[] methodArray = clazz.getDeclaredMethods();
         Object obj = clazz.getConstructor().newInstance();
-        for (Method m : methodArray) {
-            log.info("current method name:{},receive method name:{}", m.getName(), rm.getMethodName());
-            if (m.getName().equalsIgnoreCase(rm.getMethodName())) {
-                log.info("reflect invoke");
-                Object res = m.invoke(obj, new Object[]{message, mw.getMessageId()});
-                return (MessageWrapper) res;
-            }
-        }
-        return MessageWrapper.wrapperMessage(ResponseUtil.fail());
+        Method m = clazz.getMethod(rm.getMethodName(), new Class[]{String.class, String.class});
+        log.info("current method name:{}", m.getName());
+        Object res = m.invoke(obj, new Object[]{message, mw.getMessageId()});
+        return (MessageWrapper) res;
     }
 }
